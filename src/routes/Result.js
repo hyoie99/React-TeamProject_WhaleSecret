85b2d196d2522f8b8c832f19d384df04ec8a3a90
hyein <80624327+hyoie99@users.noreply.github.com>
2022-10-03 19:43:30 +0900

    add 'img capture'

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,3 +1,6 @@
+import domtoimage from "dom-to-image";
+import saveAs from "file-saver";
+import html2canvas from "html2canvas";
 import { Link, useLocation } from "react-router-dom";
 import styles from "../css/result_style.module.css";
 
@@ -5,7 +8,40 @@ function Result() {
   const location = useLocation();
   const username = location.state.username.username;
   const usertext = location.state.usertext.usertext;
-  //   console.log(username, usertext);
+  // console.log(username, usertext);
+  const saveFile = (url, filename) => {
+    let link = document.createElement("a");
+    link.href = url;
+    link.download = filename;
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  const captureImg = (event) => {
+    const card = event.target.parentNode.parentNode.parentNode.childNodes[0];
+    const card_animation = card.style.animation;
+    card.style.setProperty("animation", "none");
+    html2canvas(card, {
+      width: card.offsetWidth,
+      height: card.offsetHeight,
+      scale: 1,
+    }).then((canvas) => {
+      saveFile(canvas.toDataURL(), "card.jpg");
+      card.style.animation = card_animation;
+    });
+  };
+  // const captureImg = (event) => {
+  //   const card = event.target.parentNode.parentNode.parentNode.childNodes[0];
+  //   card.style.animation = "";
+  //   domtoimage.toBlob(card).then((blob) => {
+  //     saveAs(blob, "card.png");
+  //   });
+  //   card.style.animation = "rotate_card 9s infinite linear";
+  //   setTimeout(function () {
+  //     window.location.reload();
+  //   }, 500);
+  // };
 
   return (
     <div className={styles.main_page}>
@@ -31,7 +67,7 @@ function Result() {
           </div>
           <div className={styles.btns}>
             <div className={styles.btns_group1}>
-              <button>이미지 저장하기</button>
+              <button onClick={captureImg}>이미지 저장하기</button>
               <button>링크 공유하기</button>
             </div>
             <Link to="/ghost_page">
